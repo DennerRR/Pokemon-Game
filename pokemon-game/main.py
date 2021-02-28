f3c56cfb458675cc2ccee92841c0329fe2b0d748
Denner Ramiro Ribeiro <dennerramiror@gmail.com>
2021-02-28 02:05:02 -0300

    Pokemon Game Finished

--- pokemon-game/main.py
+++ pokemon-game/main.py
@@ -1,17 +1,15 @@
+import pickle
 from pokemon import *
 from pessoa import *
 
+
 def escolher_pokemon_inicial(player):
     print("________________________________________________________________________________")
-    print("|                                                                              |")
-    print("|                          Bem-Vindo Aventureiro                               |")
-    print("|                                                                              |")
-    print("|Olá {}, você poderá escolher o Pokemon que irá lhe acompanhar nessa jornada ! |".format(player))
+    print("|{} você deve escolher o Pokemon que irá lhe acompanhar nessa jornada ! |".format(player))
 
     bulbassauro = PokemonEletrico("Bulbassauro", level=1)
     charmander = PokemonFogo("Charmander", level=1)
     squirtle = PokemonAgua("Squirtle", level=1)
-
 
     print("|                          Você possuí 3 escolhas:                             |")
     print("|                                                                              |")
@@ -34,8 +32,67 @@ def escolher_pokemon_inicial(player):
         else:
             print("Escolha Inválida !")
 
-player = Player("Denner")
-player.capturar(PokemonFogo("Charizard", level=1))
-inimigo1 = Inimigo(nome="Gary", pokemons=[PokemonAgua("Squirtle", level=1)])
+def salvar_jogo(player):
+    try:
+        with open("database.db", "wb") as arquivo:
+            pickle.dump(player, arquivo)
+            print("Jogo salvo com sucesso!")
+    except Exception as error:
+        print("Erro ao salvar jogo")
+        print(error)
 
-player.batalhar(inimigo1)
+
+def carregar_jogo():
+    try:
+        with open("database.db", "rb") as arquivo:
+            player = pickle.load(arquivo)
+            print("Loading feito com suscesso")
+            return player
+    except Exception as error:
+        print("Save não encontrado")
+
+
+if __name__ == "__main__":
+    print("________________________________________________________________________________")
+    print("|                                                                              |")
+    print("|                          Bem-Vindo Aventureiro                               |")
+    print("|______________________________________________________________________________|")
+
+    player = carregar_jogo()
+
+    if not player:
+        nome = input("Qual Seu Nome ? ")
+        player = Player(nome)
+        print("Olá {}, Este é um mundo habitado por pokemons, a partir de agora sua missão é se tornar um mestre pokemon".format(player))
+        print("Capture o maximo de pokemons que conseguir, e lute contra seus inimigos!")
+        player.mostrar_dinheiro()
+
+        if player.pokemons:
+            print("Já vi que você tem alguns pokemons.")
+            player.mostrar_pokemons()
+
+        else:
+            print("Você não tem um pokemon, por isso precisa escolher um!")
+            escolher_pokemon_inicial(player)
+
+        print("Pronto, agora que você possui um pokemon, enfrente seu arqui-rival, Gary!")
+        gary = Inimigo(nome="Gary", pokemons=[PokemonAgua("Squirtle", level=1)])
+        player.batalhar(gary)
+        salvar_jogo(player)
+
+    while True:
+        print("---------------------------------------------")
+        print("1 - Explorar pelo mundo a fora")
+        print("2 - lutar com um inimigo")
+        print("0 - Sair do jogo")
+        escolha = input("O que deseja fazer ? ")
+
+        if escolha == "1":
+            player.explorar()
+            salvar_jogo(player)
+        elif escolha == "2":
+            inimigo_aleatorio = Inimigo()
+            player.batalhar(inimigo_aleatorio)
+            salvar_jogo(player)
+        else:
+            break
--- pokemon-game/pessoa.py
+++ pokemon-game/pessoa.py
@@ -21,13 +21,14 @@ POKEMONS = [
 
 class Pessoa():
 
-    def __init__(self, nome=None, pokemons=[]):
+    def __init__(self, nome=None, pokemons=[], dinheiro=100):
         if nome:
             self.nome = nome
         else:
             self.nome = random.choice(NOMES)
 
         self.pokemons = pokemons
+        self.dinheiro = dinheiro
 
     def __str__(self):
         return self.nome
@@ -48,6 +49,14 @@ class Pessoa():
         else:
             print("ERRO: Esse Jogador não possuí nenhum pokemon para ser escolhido ")
 
+    def mostrar_dinheiro(self):
+        print("Você possui ${}".format(self.dinheiro))
+
+    def ganhar_dinheiro(self, quantidade):
+        self.dinheiro += quantidade
+        print("Você ganhou ${}".format(quantidade))
+        self.mostrar_dinheiro()
+
     def batalhar(self, pessoa):
         print("{} iniciou uma batalha com {}".format(self, pessoa))
 
@@ -61,14 +70,16 @@ class Pessoa():
                 vitoria = pokemon.atacar(pokemon_inimigo)
                 if vitoria:
                     print("{} ganhou a batalha!!".format(self))
+                    self.ganhar_dinheiro(pokemon_inimigo.level*100)
                     break
 
-                vitoria_inimiga =  pokemon_inimigo.atacar(pokemon)
+                vitoria_inimiga = pokemon_inimigo.atacar(pokemon)
                 if vitoria_inimiga:
                     print("{} ganhou a batalha!!".format(pessoa))
                     break
         else:
             print("Essa Batalha não pode ocorrer!")
+
 
 class Player(Pessoa):
     tipo = "Player"
@@ -93,13 +104,32 @@ class Player(Pessoa):
         else:
             print("ERRO: Esse Jogador não possuí nenhum pokemon para ser escolhido ")
 
+    def explorar(self):
+        if random.random() <= 0.3:
+            pokemon = random.choice(POKEMONS)
+            print("Um pokemon Selvagem Apareceu: {}!!!!!!!".format(pokemon))
+
+            escolha = input("Deseja Capturar o Pokemon ?(s/n)")
+            if escolha == "s":
+                if random.random() > 0.5:
+                    self.capturar(pokemon)
+                else:
+                    print("{} Pokemon Fugiu!".format(pokemon))
+            else:
+                print("Ok, boa Viagem!")
+        else:
+            print("Essa Exploração nao deu em nada! ")
+
 
 class Inimigo(Pessoa):
     tipo = "Inimigo"
 
-    def __init__(self, nome=None, pokemons=[]):
+    def __init__(self, nome=None, pokemons=None):
         if not pokemons:
+            pokemons_aleatorios = []
             for i in range(random.randint(1, 6)):
-                pokemons.append(random.choice(POKEMONS))
+                pokemons_aleatorios.append(random.choice(POKEMONS))
+            super().__init__(nome=nome, pokemons=pokemons_aleatorios)
 
-        super().__init__(nome=nome, pokemons=pokemons)
+        else:
+            super().__init__(nome=nome, pokemons=pokemons)
